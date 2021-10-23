7dd75ccedc6992680304b05667938391340f24f7
AbangTor <63029179+abangtor@users.noreply.github.com>
2021-10-23 22:06:22 +0800

    Added Start on external power

--- UPSLite.py
+++ UPSLite.py
@@ -15,30 +15,43 @@ class UPSLite:
     self.flVoltage   = 0
     self.flCapacity  = 0
     self.boIsOnPower = False
+    self.boInitBus   = True
     
-    # init GPIO
+    # Init GPIO
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(self.uiGpioId, GPIO.IN)
-    
-    # init i2c Bus
-    self.objBus.write_word_data(self.uiAddress, 0xfe,0x0054)
-    self.objBus.write_word_data(self.uiAddress, 0x06,0x4000)
-    time.sleep(1)
     
   def Update(self):
     # Read power suppy state
     self.boIsOnPower = (GPIO.input(self.uiGpioId) == GPIO.HIGH)
     
-    # Read voltage from i2c
-    ui16BusReading   = self.objBus.read_word_data(self.uiAddress, 0X02)
-    flVoltageRaw     = struct.unpack("<H", struct.pack(">H", ui16BusReading))[0]
-    self.flVoltage   = flVoltageRaw * 1.25 /1000/16
+    if( self.boIsOnPower ):
+      # Enforce initialisation of bus un next run
+      if( not self.boInitBus ):
+        self.boInitBus = True
+    else:
+      # Init bus if necesarry
+      if( self.boInitBus ):
+        self.InitBus()
     
-    # Read capacity from i2c
-    ui16BusReading   = self.objBus.read_word_data(self.uiAddress, 0X04)
-    flCapacityRaw    = struct.unpack("<H", struct.pack(">H", ui16BusReading))[0]
-    self.flCapacity  = flCapacityRaw/256
+      # Read voltage from i2c
+      ui16BusReading   = self.objBus.read_word_data(self.uiAddress, 0X02)
+      flVoltageRaw     = struct.unpack("<H", struct.pack(">H", ui16BusReading))[0]
+      self.flVoltage   = flVoltageRaw * 1.25 /1000/16
+      
+      # Read capacity from i2c
+      ui16BusReading   = self.objBus.read_word_data(self.uiAddress, 0X04)
+      flCapacityRaw    = struct.unpack("<H", struct.pack(">H", ui16BusReading))[0]
+      self.flCapacity  = flCapacityRaw/256
+    
+  def InitBus(self):
+    # Init i2c Bus
+    self.objBus.write_word_data(self.uiAddress, 0xfe,0x0054)
+    self.objBus.write_word_data(self.uiAddress, 0x06,0x4000)
+    time.sleep(1)
+    # Set bus as initialized
+    self.boInitBus = False
     
   def IsOnPower(self):
     return self.boIsOnPower
@@ -59,9 +72,11 @@ objUpsLite = UPSLite(1, 0x36, 4)
 while True:
   objUpsLite.Update();
   
-  print "<7> %5.2fV" % objUpsLite.GetVoltage() \
-      + " %5i%%" % objUpsLite.GetCapacity() \
-      + ({True: " Plugged In", False: ""}[objUpsLite.IsOnPower()])
+  if( objUpsLite.IsOnPower() ):
+    print "<7> On external Powersupply..."
+  else:
+    print "<7> On Battery: %5.2fV" % objUpsLite.GetVoltage() \
+        + " %5i%%" % objUpsLite.GetCapacity()
   
   if( not objUpsLite.IsOnPower() and \
       objUpsLite.GetCapacity() <= 30 ):
